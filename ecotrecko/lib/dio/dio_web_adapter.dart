d413c41556a5a98d150a44b54ae09b883b13a3ee
wiirijofct <y.hayduk@campus.fct.unl.pt>
2024-11-21 20:55:31 +0000

    apaguei merdas

--- ecotrecko/lib/dio/dio_web_adapter.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/browser.dart';
-import 'package:dio/dio.dart';
-
-Future<Dio> createDio2() {
-  var dio = Dio();
-  dio.httpClientAdapter = BrowserHttpClientAdapter(withCredentials: true);
-  return Future(() => dio);
-}
--- ecotrecko/lib/file_selectors/file_selector.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:typed_data';
-
-abstract class FileSelector {
-  Future<Uint8List?> selectFile();
-}
